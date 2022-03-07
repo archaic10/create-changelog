c3287bb45c16bbfc739bbae5a244a9875491342f
archaic10 <vitor.franca.9400@gmail.com>
2022-03-07 18:13:16 -0300

    feat: decouple

--- change.sh
+++ change.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo "teste"
-yarn install
-npm version patch
-yarn add auto-changelog --dev
-echo yarn auto-changelog -p  >> CHANGELOG.md 
+# npm install
+# npm version patch
+npm install auto-changelog --save-dev
+
+auto-changelog -p
